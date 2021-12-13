7b35a2837edca69e7f5558a9dec9bb7d1fbcce48
Imam Nur Arifin <imam.12ra.kkpi@gmail.com>
2021-12-13 11:59:29 +0700

    navbar

---
+++ components/Button.js
@@ -0,0 +1,7 @@
+export default function Button({ children }) {
+  return (
+    <a className="border border-yelllow-500 text-yelllow-500 py-3 px-10 font-semibold text-lg rounded-full">
+      {children}
+    </a>
+  )
+};
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        yelllow: {
+          "50": "#ff100109",
+          "100": "#fffdf0",
+          "200": "#fef8be",
+          "300": "#fdf390",
+          "400": "#fded5e",
+          "500": "#fce72c",
+          "600": "#e7d103",
+          "700": "#ab9a02",
+          "800": "#6f6402",
+          "900": "#322d01"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
